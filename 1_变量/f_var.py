699ec55113b448c91d2ff467b8962331fb8df16c
wu <1191297340@qq.com>
2021-05-23 22:59:02 +0800

    添加字符串相关操作

---
+++ 1_变量/f_var.py
@@ -0,0 +1,34 @@
+# 整型
+var_int = 100
+print(var_int)
+
+# 浮点
+var_float = 100.0
+print(var_float)
+
+# 字符串
+var_string = "This is string"
+print(var_string)
+
+# 字符串截取
+print(var_string[1:3])
+print(var_string[1:])
+print(var_string*2)  
+print(var_string + "haha")
+
+# 字符串查找
+print("is" in var_string)
+print("is" not in var_string)
+
+print(var_string.find("s"))  # 字符串首次出现的位置
+print(var_string.rfind("s")) # 字符串最后一次出现的位置
+print(var_string.index("s")) 
+print(var_string.replace("string","hello"))
+print(var_string.lower())
+print(var_string.upper())
+
+# list
+
+# Tuple
+
+# Dictionary
